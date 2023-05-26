473284e0ac21ec88515ed4189047a12673e62dbd
shubham181196 <87092826+shubham181196@users.noreply.github.com>
2023-05-26 17:40:51 +0530

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { ServerConfig } = require('./config');
+const { ServerConfig,Logger } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
@@ -9,4 +9,5 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    Logger.info("server started succesfully");
 });
